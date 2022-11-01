ca816f32d14ce810f9b46c3c4ba136a0dffa3faf
mb3ch3 <33735505+mb3ch3@users.noreply.github.com>
2022-11-01 22:57:46 +0300

    Update 2-main.py

--- 0x16-api_advanced/tests/2-main.py
+++ 0x16-api_advanced/tests/2-main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
+    if result is not None:
+        print(len(result))
+    else:
+        print("None")
